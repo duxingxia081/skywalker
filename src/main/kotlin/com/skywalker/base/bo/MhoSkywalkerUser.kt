c2c377f518055ba20529d5a8f5bc62e832db4bd9
duxingxia081 <332931496@qq.com>
2018-05-16 20:44:48 +0800

    初始提交

--- src/main/kotlin/com/skywalker/base/bo/MhoSkywalkerUser.kt
+++ src/main/kotlin/com/skywalker/base/bo/MhoSkywalkerUser.kt
@@ -6,24 +6,22 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
-data class MhoSkywalkerUser(
-
+data class MhoSkywalkerUser constructor(
         @Id
         @GeneratedValue
-        var userId: Long,
-        var userName: String,
-        var mobilePhone: String,
-        var wechatId: String,
-        var qqId: String,
-        var password: String,
-        var nickname: String,
-        var sign: String,
-        var sex: String,
-        var address: String,
-        var qrCodeImage: String,
-        var headImage: String,
-        var coverImage: String,
-        var timeCreate: Date,
-        var isDelete: String
-
+        var userId: Long=0L,
+        var userName: String="",
+        var mobilePhone: String="",
+        var wechatId: String="",
+        var qqId: String="",
+        var password: String="",
+        var nickname: String="",
+        var sign: String="",
+        var sex: String="",
+        var address: String="",
+        var qrCodeImage: String="",
+        var headImage: String="",
+        var coverImage: String="",
+        var timeCreate: Date=Date(),
+        var isDelete: String=""
 )
