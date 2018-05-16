d1a133660d9f76f6ef741babca1102773d4125fe
duxingxia081 <332931496@qq.com>
2018-05-16 18:23:32 +0800

    优化

--- src/main/kotlin/com/skywalker/user/web/UserController.kt
+++ src/main/kotlin/com/skywalker/user/web/UserController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping("/api/users")
 class UserController(private val userService: UserService){
     @GetMapping("/findUser/{userId}")
     fun findByLastName(@PathVariable userId:String)
