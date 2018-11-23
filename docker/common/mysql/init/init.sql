fa632128b855110da3174084a1eea6ff6fbf2bc7
duxingxia081 <332931496@qq.com>
2018-11-23 17:43:02 +0800

    更改说明

--- docker/common/mysql/init/init.sql
+++ docker/common/mysql/init/init.sql
@@ -1,2 +1,3 @@
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '666666' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'skywalker'@'%' IDENTIFIED BY '666666' WITH GRANT OPTION;
+alter user 'skywalker'@'localhost' identified with mysql_native_password by '666666';
