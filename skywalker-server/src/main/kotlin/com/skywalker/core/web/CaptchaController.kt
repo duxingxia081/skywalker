ebf27b95b9abbe45e0f49927c45a2fed27dab202
duxingxia081 <332931496@qq.com>
2018-12-05 20:02:01 +0800

    修改人员信息

---
+++ skywalker-server/src/main/kotlin/com/skywalker/core/web/CaptchaController.kt
@@ -0,0 +1,25 @@
+package com.skywalker.core.web
+
+import cn.hutool.captcha.CaptchaUtil
+import com.skywalker.core.response.SuccessResponse
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.awt.Color
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@RestController
+@RequestMapping("/captcha")
+@CrossOrigin
+class CaptchaController {
+    @GetMapping
+    fun getKaptcha(response: HttpServletResponse, request: HttpServletRequest): SuccessResponse {
+        //定义图形验证码的长和宽
+        val lineCaptcha = CaptchaUtil.createCircleCaptcha(200, 100, 4, 50)
+        lineCaptcha.setBackground(Color(220, 106, 169))
+        request.session.setAttribute("captcha",lineCaptcha.code)
+        return SuccessResponse(lineCaptcha.imageBase64)
+    }
+}
