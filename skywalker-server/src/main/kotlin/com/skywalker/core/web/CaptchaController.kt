cd55b9d76f002974bc74544ac978a5c845a7a906
duxingxia081 <332931496@qq.com>
2018-12-06 21:24:59 +0800

    修改人员信息

--- skywalker-server/src/main/kotlin/com/skywalker/core/web/CaptchaController.kt
+++ skywalker-server/src/main/kotlin/com/skywalker/core/web/CaptchaController.kt
@@ -1,25 +1,38 @@
 package com.skywalker.core.web
 
 import cn.hutool.captcha.CaptchaUtil
+import cn.hutool.core.codec.Base64
+import cn.hutool.extra.qrcode.QrCodeUtil
+import com.skywalker.auth.utils.JwtTokenUtil
+import com.skywalker.core.constants.ErrorConstants
+import com.skywalker.core.exception.ServiceException
 import com.skywalker.core.response.SuccessResponse
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.skywalker.user.dto.UserDTO
+import com.skywalker.user.service.UserService
+import org.springframework.web.bind.annotation.*
 import java.awt.Color
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @RestController
-@RequestMapping("/captcha")
-@CrossOrigin
-class CaptchaController {
-    @GetMapping
+class CaptchaController(private val jwtTokenUtil: JwtTokenUtil,
+                        private val userService: UserService) {
+    @GetMapping("/captcha")
     fun getKaptcha(response: HttpServletResponse, request: HttpServletRequest): SuccessResponse {
         //定义图形验证码的长和宽
-        val lineCaptcha = CaptchaUtil.createCircleCaptcha(200, 100, 4, 50)
+        val lineCaptcha = CaptchaUtil.createCircleCaptcha(100, 30, 4, 10)
         lineCaptcha.setBackground(Color(220, 106, 169))
-        request.session.setAttribute("captcha",lineCaptcha.code)
-        return SuccessResponse("data:image/gif;base64,"+lineCaptcha.imageBase64)
+        request.session.setAttribute("captcha", lineCaptcha.code)
+        return SuccessResponse("data:image/gif;base64," + lineCaptcha.imageBase64)
+    }
+
+    @GetMapping("/qrCode")
+    fun getQrCode(response: HttpServletResponse, request: HttpServletRequest): SuccessResponse {
+        val userId = jwtTokenUtil.getUserIdFromToken(request) ?: throw ServiceException(
+                ErrorConstants.ERROR_CODE_1104,
+                ErrorConstants.ERROR_MSG_1104
+        )
+        val user: UserDTO = userService.findById(userId)
+        return SuccessResponse("data:image/gif;base64," + Base64.encode(QrCodeUtil.generatePng(user.userName, 300, 300)))
     }
 }
