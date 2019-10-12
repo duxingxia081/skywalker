448f877893596fb8e05bfe4b5630ee159fed92a6
duxingxia081 <332931496@qq.com>
2019-10-12 14:32:27 +0800

    修改脚本

--- docker/common/mysql/init/init.sql
+++ docker/common/mysql/init/init.sql
@@ -1,4 +1,4 @@
-alter user 'skywalker' identified with mysql_native_password by 'Jade_19960902';
-set password for 'root'@'%'=password('Jade_19960902');
+CREATE USER 'skywalker'@'%' IDENTIFIED WITH mysql_native_password BY 'Jade_19960902';
+ALTER USER 'root'@'%' IDENTIFIED BY 'Jade_19960902'
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'skywalker'@'%';
